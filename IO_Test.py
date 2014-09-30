d3c1cdcbb604b87f8ff693577baf349afc71f8b3
Arlo012 <arlo012@gmail.com>
2014-09-29 21:23:09 -0400

    Test push to show Jorge operation of SmartGit.

--- IO_Test.py
+++
@@ -1,4 +0,0 @@
-#Test of python terminal IO
-
-username = input("Hello, this is your Raspberry Pi. Please provide your name or be annihilated.")
-print ("It is my pleasure to meet you " + username + ", have a nice day")
