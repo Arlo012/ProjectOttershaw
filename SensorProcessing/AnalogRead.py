9c561393704ab0d4ef4f9b9fb26d71259163df7d
Arlo012 <arlo012@gmail.com>
2014-10-22 22:33:17 -0400

    Beginning to integrate sensor reading from python. Sonic scan class specifically for the sonic scanner, and analog read generic. More updates to come

---
+++ SensorProcessing/AnalogRead.py
@@ -0,0 +1,9 @@
+class AnalogRead:
+    def __init__(self, name, pin):
+        self.name = name
+        self.pin = pin
+    
+    def read(self):
+        '''Return value from analog pin'''
+        pass
+    
