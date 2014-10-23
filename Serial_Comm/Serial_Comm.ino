bb3bb673a039ce395865696f7ec9fa435f382a2f
TheXhero <xhero91@gmail.com>
2014-10-23 00:29:36 -0400

    Brian test commit
    First serial comm commit

---
+++ Serial_Comm/Serial_Comm.ino
@@ -0,0 +1,47 @@
+void setup()
+{
+
+}
+char command_char;
+char command[20];
+int Spidey_Sense()
+{
+
+}
+int get_command(char* list)
+{
+	int index = 0;
+	while (Serial.available()>0)
+	{
+		if (index < 19)
+		{
+			command_char = Serial.read();
+			command[index] = command_char;
+			index++;
+			command[index] = '\0';
+		}
+	}
+	if (strcmp(command, list) == 0)
+	{
+		for (int i = 0; i<19; i++)
+		{
+			command[i] = 0;
+		}
+		index = 0;
+		return(0);
+	}
+	else
+	{
+		return(1);
+	}
+}
+
+void loop()
+{
+
+	if (get_command("spideysense") == 0)
+	{
+		Serial.write(Spidey_Sense());
+	}
+
+}
