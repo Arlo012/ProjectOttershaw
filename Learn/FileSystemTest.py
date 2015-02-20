297fb0230f604036d7c59c50587777627f0ae72d
Arlo012 <arlo012@gmail.com>
2015-02-20 12:58:56 -0500

    Begin cleanup of project file structure

--- Learn/FileSystemTest.py
+++
@@ -1,6 +0,0 @@
-import os
-from pathlib import Path
-
-#Print my filename and absolute path
-print(os.getcwd())
-print("filename " + __file__)
--- SensorProcessing/AnalogRead.py
+++
@@ -1,9 +0,0 @@
-class AnalogRead:
-    def __init__(self, name, pin):
-        self.name = name
-        self.pin = pin
-    
-    def read(self):
-        '''Return value from analog pin'''
-        pass
-    
